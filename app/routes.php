691b2657524c13c4ec5a06800a02f8bbbcb88e3c
katzhang <arcadychang@gmail.com>
2014-04-13 21:27:56 -0400

    added controllers

--- app/routes.php
+++ app/routes.php
@@ -16,7 +16,13 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('/authtest', array('before' => 'auth.basic', function()
+// Route::get('/authtest', array('before' => 'auth.basic', function()
+// {
+//     return View::make('hello');
+// }));
+
+// Route group for API versioning
+Route::group(array('prefix' => 'api/v1', 'before' => 'auth.basic'), function()
 {
-    return View::make('hello');
-}));
+    Route::resource('url', 'UrlController');
+});
